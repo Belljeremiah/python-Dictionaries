c6c11145ab91a4b149f2a9aeba3dc3907214556e
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2020-01-16 14:49:56 -0600

    finished exercises

--- dictionaryOfWords.py
+++ dictionaryOfWords.py
@@ -9,5 +9,26 @@ my_dictionary["succinct"] = "(especially of something written or spoken) briefly
 my_dictionary["alabaster"] = "a fine-grained, translucent form of gypsum, typically white, often carved"
 my_dictionary["Phencyclidine"] = "angel dust, mind altering substance"
 
+print(my_dictionary['truncated'], my_dictionary['succinct'])
+
 for (key, value) in my_dictionary.items():
-    print(f'{key}: {value}')
+    print(f"The Definition of {key}: is {value}")
+
+# End of Practice One
+# Practice: English Idioms
+
+idioms = {
+    "Penny": ["A", "penny", "for", "your", "thoughts"],
+    "Injury": ["Add", "insult", "to", "injury"],
+    "Moon": ["Once", "in", "a", "blue", "moon"],
+    "Grape": ["I", "heard", "it", "through", "the", "grapevine"],
+    "Murder": ["Kill", "two", "birds", "with", "one", "stone"],
+    "Limbs": ["It", "costs", "an", "arm", "and", "a", "leg"],
+    "Grain": ["Take", "what", "someone", "says", "with", "a", "grain", "of", "salt"],
+    "Fences": ["I'm", "on", "the", "fence", "about", "it"],
+    "Sheep": ["Pulled", "the", "wool", "over", "his", "eyes"],
+    "Lucifer": ["Speak", "of", "the", "devil"],
+}
+
+for a in idioms:
+    print(a, idioms[a])
